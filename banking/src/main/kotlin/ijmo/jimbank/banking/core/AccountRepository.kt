4fdb5e6b8b68e8c907a9d893de11ecc58996ea9a
ijmo <jinmo.im@gmail.com>
2020-02-06 23:37:41 +0900

    Add repository method declarations and service methods

--- banking/src/main/kotlin/ijmo/jimbank/banking/core/AccountRepository.kt
+++ banking/src/main/kotlin/ijmo/jimbank/banking/core/AccountRepository.kt
@@ -3,5 +3,6 @@ package ijmo.jimbank.banking.core
 import org.springframework.data.repository.CrudRepository
 
 interface AccountRepository : CrudRepository<Account, Long> {
-//    fun findAllByOwnerId(ownerId: Long): List<Account>
+    fun findAllByOwnerId(ownerId: Long): List<Account>
+    fun findByAccountNo(accountNo: String): Account?
 }
--- banking/src/main/kotlin/ijmo/jimbank/banking/core/BankingService.kt
+++ banking/src/main/kotlin/ijmo/jimbank/banking/core/BankingService.kt
@@ -1,6 +1,7 @@
 package ijmo.jimbank.banking.core
 
 import org.springframework.stereotype.Service
+import javax.transaction.Transactional
 
 @Service
 class BankingService (private val accountRepository: AccountRepository) {
@@ -12,47 +13,88 @@ class BankingService (private val accountRepository: AccountRepository) {
     /**
      * 계좌 신규
      */
-    fun create(ownerId: Long, amount: Double) {
-        accountRepository.save(Account.Builder().accountNo(generateNewAccountNo()).balance(amount).build())
+    fun create(ownerId: Long, amount: Double): Account? {
+        return accountRepository.save(Account.Builder().accountNo(generateNewAccountNo()).balance(amount).build())
     }
 
-    fun create(ownerId: Long) {
-        create(ownerId, 0.0)
+    fun create(ownerId: Long): Account? {
+        return create(ownerId, 0.0)
     }
 
     /**
      * 계좌 조회
      */
     fun list(ownerId: Long): List<Account> {
-//        return accountRepository.findAllByOwnerId(ownerId)
-        return emptyList()
+        return accountRepository.findAllByOwnerId(ownerId)
     }
+
+    fun findByAccountNo(accountNo: String): Account? = accountRepository.findByAccountNo(accountNo)
 
     /**
      * 계좌 상태 변경
      */
-    fun update() {
-//        accountRepository.
+    fun update(account: Account, enabled: Boolean): Account? {
+        account.enabled = enabled
+        return accountRepository.save(account)
+    }
+
+    fun update(accountNo: String, enabled: Boolean): Account? {
+        val account = findByAccountNo(accountNo) ?: throw AccountNotFoundException("")
+        return update(account, enabled)
     }
 
     /**
      * 입금
      */
-    fun deposit(accountNo: String) {
-//        accountRepository.
+    @Transactional
+    fun deposit(account: Account, amount: Double): Account? {
+        account.balance = account.balance ?: 0 + amount
+        return accountRepository.save(account)
+    }
+
+    @Transactional
+    fun deposit(accountNo: String, amount: Double): Account? {
+        val account = findByAccountNo(accountNo) ?: throw AccountNotFoundException("")
+        return deposit(account, amount)
     }
 
     /**
      * 출금
      */
-    fun withdraw() {
+    @Transactional
+    fun withdraw(account: Account, amount: Double): Account? {
+        if (account.balance == null || account.balance!! < amount) {
+            throw NotEnoughMoneyException("")
+        }
+        account.balance = account.balance!! - amount
+        return accountRepository.save(account)
+    }
 
+    @Transactional
+    fun withdraw(accountNo: String, amount: Double): Account? {
+        val account = findByAccountNo(accountNo) ?: throw AccountNotFoundException("")
+        return withdraw(account, amount)
     }
 
     /**
      * 이체
      */
-    fun wire() {
+    @Transactional
+    fun wire(accountSending: Account, accountReceiving: Account, amount: Double): Account? {
+        if (accountSending.balance == null || accountSending.balance!! < amount) {
+            throw NotEnoughMoneyException("")
+        }
+        accountSending.balance = accountSending.balance!! - amount
+        accountReceiving.balance = accountReceiving.balance ?: 0 + amount
+        accountRepository.save(accountSending)
+        accountRepository.save(accountReceiving)
+        return accountSending
+    }
 
+    @Transactional
+    fun wire(accountNoSending: String, accountNoReceiving: String, amount: Double): Account? {
+        val accountSending = findByAccountNo(accountNoSending) ?: throw AccountNotFoundException("")
+        val accountReceiving = findByAccountNo(accountNoReceiving) ?: throw AccountNotFoundException("")
+        return wire(accountSending, accountReceiving, amount)
     }
 }
