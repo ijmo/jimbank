2d4d46b19587d6f66a62902a55b420210dff6d06
ijmo <jinmo.im@gmail.com>
2020-02-07 22:52:03 +0900

    Add clerk_id column

--- banking/src/main/kotlin/ijmo/jimbank/banking/core/AccountHistory.kt
+++ banking/src/main/kotlin/ijmo/jimbank/banking/core/AccountHistory.kt
@@ -19,31 +19,37 @@ class AccountHistory : BaseEntity {
     @Column(name = "amount")
     var amount: Double? = null
 
+    @Column(name = "clerk_id")
+    var clerkId: String? = null
+
     @Column(name = "created_on")
     @CreatedDate
     var createdOn: LocalDateTime? = LocalDateTime.now()
 
-    private constructor(accountNo: String?, accountNoReceived: String?, amount: Double?) : this() {
+    private constructor(accountNo: String?, accountNoReceived: String?, amount: Double?, clerkId: String?) : this() {
         this.accountNo = accountNo
         this.accountNoReceived = accountNoReceived
         this.amount = amount
+        this.clerkId = clerkId
     }
 
     constructor()
 
-    override fun toString() = "Account(accountNo=$accountNo, accountNoReceived=$accountNoReceived, amount=$amount)"
+    override fun toString() = "Account(accountNo=$accountNo, accountNoReceived=$accountNoReceived, amount=$amount, clerkId=$clerkId)"
 
     data class Builder(
         private var accountNo: String? = null,
         private var accountNoReceived: String? = null,
-        private var amount: Double? = null) {
+        private var amount: Double? = null,
+        private var clerkId: String? = null) {
 
         fun accountNo(accountNo: String) = apply { this.accountNo = accountNo }
         fun accountNoReceived(accountNoReceived: String) = apply { this.accountNoReceived = accountNoReceived }
         fun amount(amount: Double) = apply { this.amount = amount }
-        fun build() = AccountHistory(accountNo, accountNoReceived, amount)
+        fun clerkId(clerkId: String?) = apply { this.clerkId = clerkId }
+        fun build() = AccountHistory(accountNo, accountNoReceived, amount, clerkId)
 
         override fun toString() =
-            "AccountHistory.Builder(accountNo=$accountNo, accountNoReceived=$accountNoReceived, amount=$amount)"
+            "AccountHistory.Builder(accountNo=$accountNo, accountNoReceived=$accountNoReceived, amount=$amount, clerkId=$clerkId)"
     }
 }
