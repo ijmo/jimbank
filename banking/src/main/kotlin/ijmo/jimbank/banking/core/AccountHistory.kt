c0333bde791efef5d473506b54fc1e8a328e7160
ijmo <jinmo.im@gmail.com>
2020-02-13 23:49:49 +0900

    Add fields

--- banking/src/main/kotlin/ijmo/jimbank/banking/core/AccountHistory.kt
+++ banking/src/main/kotlin/ijmo/jimbank/banking/core/AccountHistory.kt
@@ -19,6 +19,12 @@ class AccountHistory : BaseEntity {
     @Column(name = "amount")
     var amount: Double? = null
 
+    @Column(name = "message")
+    var message: String? = null
+
+    @Column(name = "note")
+    var note: String? = null
+
     @Column(name = "clerk_id")
     var clerkId: String? = null
 
@@ -26,30 +32,36 @@ class AccountHistory : BaseEntity {
     @CreatedDate
     var createdOn: LocalDateTime? = LocalDateTime.now()
 
-    private constructor(accountNo: String?, accountNoReceived: String?, amount: Double?, clerkId: String?) : this() {
+    private constructor(accountNo: String?, accountNoReceived: String?, amount: Double?, message: String?, note: String?, clerkId: String?) : this() {
         this.accountNo = accountNo
         this.accountNoReceived = accountNoReceived
         this.amount = amount
+        this.message = message
+        this.note = note
         this.clerkId = clerkId
     }
 
     constructor()
 
-    override fun toString() = "Account(accountNo=$accountNo, accountNoReceived=$accountNoReceived, amount=$amount, clerkId=$clerkId)"
+    override fun toString() = "AccountHistory(accountNo=$accountNo, accountNoReceived=$accountNoReceived, amount=$amount, message=$message, note=$note, clerkId=$clerkId)"
 
     data class Builder(
         private var accountNo: String? = null,
         private var accountNoReceived: String? = null,
         private var amount: Double? = null,
+        private var message: String? = null,
+        private var note: String? = null,
         private var clerkId: String? = null) {
 
         fun accountNo(accountNo: String) = apply { this.accountNo = accountNo }
         fun accountNoReceived(accountNoReceived: String) = apply { this.accountNoReceived = accountNoReceived }
         fun amount(amount: Double) = apply { this.amount = amount }
+        fun message(message: String) = apply { this.message = message }
+        fun note(note: String) = apply { this.note = note }
         fun clerkId(clerkId: String?) = apply { this.clerkId = clerkId }
-        fun build() = AccountHistory(accountNo, accountNoReceived, amount, clerkId)
+        fun build() = AccountHistory(accountNo, accountNoReceived, amount, message, note, clerkId)
 
         override fun toString() =
-            "AccountHistory.Builder(accountNo=$accountNo, accountNoReceived=$accountNoReceived, amount=$amount, clerkId=$clerkId)"
+            "AccountHistory.Builder(accountNo=$accountNo, accountNoReceived=$accountNoReceived, amount=$amount, message=$message, note=$note, clerkId=$clerkId)"
     }
 }
